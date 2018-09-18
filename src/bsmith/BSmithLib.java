f31c67f58f5906f5839967b1f31d782e8531f482
BT_1N3_07 <bsmith3816@bths.edu>
2018-09-18 09:20:30 -0400

    Template

---
+++ src/bsmith/BSmithLib.java
@@ -0,0 +1,7 @@
+package bsmith;
+
+public class BSmithLib {
+    public static void println(String str){
+        System.out.println(str);
+    }
+}
---
+++ src/bsmith/TestFile.java
@@ -0,0 +1,12 @@
+package bsmith;
+
+import static bsmith.BSmithLib.println;
+
+public class TestFile {
+
+    public static void main(String[] args)
+    {
+        println("I love a cup of Java in the morning.");
+    }
+
+}
